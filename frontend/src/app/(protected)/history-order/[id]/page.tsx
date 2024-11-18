4be47c658dce5c15f3945de38862fa7c406d31c9
idkwattuput <ikhwank322@gmail.com>
2024-11-19 03:38:37 +0800

    feat: history order logic

---
+++ frontend/src/app/(protected)/history-order/[id]/page.tsx
@@ -0,0 +1,113 @@
+"use client"
+
+import useAxiosPrivate from "@/hooks/use-axios-private"
+import { Order } from "@/types/order"
+import { useEffect, useState } from "react"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { useParams } from "next/navigation";
+import SkeletonWrapper from "@/components/skeleton-wrapper";
+import { Cafe } from "@/types/cafe";
+import { MapPin } from "lucide-react";
+
+interface CustomOrder extends Order {
+  cafe: Cafe
+}
+
+export default function OrderById() {
+  const params = useParams<{ tag: string; id: string }>()
+  const axiosPrivate = useAxiosPrivate()
+  const [order, setOrder] = useState<CustomOrder | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function getOrder(id: string) {
+      try {
+        setLoading(true)
+        const response = await axiosPrivate.get(`/api/v1/orders/${id}`)
+        setOrder(response.data.data)
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        console.log(error)
+      }
+    }
+    getOrder(params.id)
+  }, [params.id])
+
+  if (loading) {
+    return (
+      <div className="p-4">
+        <SkeletonWrapper isLoading={loading}>
+          <Card>
+            <CardHeader>
+              <CardTitle>Loading</CardTitle>
+              <CardDescription>Loading</CardDescription>
+            </CardHeader>
+            <CardContent>Loading</CardContent>
+          </Card>
+        </SkeletonWrapper>
+      </div>
+    )
+  }
+
+  return (
+    <div className="p-4">
+      {order ? (
+        <div>
+          <StatusText status={order.status} />
+          <p className="mt-4">Order Details</p>
+          <div>
+            <p>Order ID: {order.id}</p>
+            <p>Cafe: {order.cafe.name}</p>
+            <p className="flex items-center">
+              <MapPin className="w-4 h-4" />
+              {order.cafe.block}-{order.cafe.lot}
+            </p>
+          </div>
+          <div>
+            {order.OrderItems.map((item) => (
+              <div key={item.id} className="flex gap-4">
+                <div>
+                  <p>{item.quantity}x {item.item.name}</p>
+                  <p>{item.note}</p>
+                </div>
+                <p>RM {Number(Number(item.item.price) * item.quantity).toFixed(2)}</p>
+              </div>
+            ))}
+          </div>
+          <p className="font-bold">Total: RM {Number(order.total_price).toFixed(2)}</p>
+        </div>
+      ) : (
+        <div>Order not found</div>
+      )}
+    </div>
+  )
+}
+
+function StatusText({ status }: { status: "PENDING" | "PREPARING" | "COMPLETED" | "CANCELLED" }) {
+  return (
+    <h1 className="text-3xl font-bold">
+      {status === "PENDING" && (
+        <>
+          We&apos;re reviewing your order details. Please hold on!
+        </>
+      )}
+      {status === "PREPARING" && (
+        <>
+          Your order is being prepared. Stay tuned for updates!
+        </>
+      )}
+      {status === "COMPLETED" && (
+        <>
+          Order completed successfully. Enjoy your meal!
+        </>
+      )}
+      {status === "CANCELLED" && (
+        <>
+          This order has been cancelled
+        </>
+      )}
+    </h1>
+  )
+}
+
