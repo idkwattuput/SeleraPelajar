148e08929d0482aa473da3519ab2f134c1c863a0
idkwattuput <ikhwank322@gmail.com>
2025-01-24 18:31:10 +0800

    refactor(seller): make it can add tailwindcss className

--- frontend-seller/src/components/skeleton-wrapper.tsx
+++ frontend-seller/src/components/skeleton-wrapper.tsx
@@ -6,14 +6,16 @@ export default function SkeletonWrapper({
   children,
   isLoading,
   fullWidth = true,
+  className = ""
 }: {
   children: ReactNode
   isLoading: boolean
   fullWidth?: boolean
+  className?: string
 }) {
   if (!isLoading) return children
   return (
-    <Skeleton className={cn(fullWidth && "w-full")}>
+    <Skeleton className={cn(fullWidth && "w-full", className)}>
       <div className="opacity-0">{children}</div>
     </Skeleton>
   )
